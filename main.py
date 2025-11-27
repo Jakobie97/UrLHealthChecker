9d70b0e17b69839f9c16f1f37e175819dea6b0b1
Jacob Stockwell <JacobtStockwell@gmail.com>
2025-11-27 11:01:14 -0600

    Spelling
    cleaned up imports
    
    added connection string to db
    
    Each output is now logged in a local sqlite db

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-import requests
-import time
-import datetime
+import requests, time, datetime, sqlite3
 
-url = 'https://status.playstation.com/'
+
+
+
+connection = sqlite3.connect('./Database/MyUrlChecksDBStorage.db') #path to sql db file
+cursor = connection.cursor() #helper that inserts the data
+
+sqlInsertQuery = "INSERT INTO Url_Responses (url, status_code, timestamp) VALUES (?, ?, ?);"
+  
 
 myURLs= ["https://status.playstation.com/",
-         "https://slack-status.com/","https://health.aws.amazon.com/health/status",
-         "https://status.cloud.microsoft/m365/referrer=serviceStatusRedirect"]
+        "https://slack-status.com/","https://health.aws.amazon.com/health/status",
+        "https://status.cloud.microsoft/m365/referrer=serviceStatusRedirect"]
 
 
 for i in myURLs:
     
-    urlResponse = requests.get(url)
-    print(urlResponse)
+    urlResponse = requests.get(i)
+    
 
     #this block takes the raw time data in second and make it more readaable 
     currentTimestamp = time.time()
@@ -22,11 +27,26 @@ for i in myURLs:
     readable_time_string = dt_object.strftime("%Y-%m-%d %H:%M:%S") 
     
     if urlResponse.status_code == 200: # 200 mean OK
+        
         print(i)
         print(readable_time_string)
-        print('Your site is onliine!')
+        print('Your site is online!')
+
+        cursor.execute(sqlInsertQuery,(i,urlResponse.status_code,readable_time_string)) 
         print('')
+        
     else:
+
         print(i)
-        print('The site is down go touch grass')
+        print(readable_time_string)
+        print('The site is down. Go touch grass')
+
+        cursor.execute(sqlInsertQuery,(i,urlResponse.status_code,readable_time_string))
         print('')
+
+connection.commit() #pushes to database
+
+print("Your database has successfully logged theses entries! :)")
+connection.close()
+    
+  
